adcb72031af59591f9a3c86d7976f52e3689a216
gsrnandan <raghunandang.89@gmail.com>
2017-05-04 21:50:57 +0530

    Delete lightManager_Mach2.py

--- lightManager_Mach2.py
+++
@@ -1,8 +0,0 @@
-if __name__ == '__main__':
-     
-          
-    pointer = mui.MQtUtil.mainWindow()    
-    win = WndTutorial05()
-    win.setWindowTitle("LightLeBhai")
-    win.setStyle(QtGui.QStyleFactory.create('Cleanlooks'))
-    win.show()
