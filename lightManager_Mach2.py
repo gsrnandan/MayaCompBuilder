854c7690f30bdf0f0a777058a40160c8b08aec66
gsrnandan <raghunandang.89@gmail.com>
2017-05-04 21:49:57 +0530

    Add files via upload

---
+++ lightManager_Mach2.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+     
+          
+    pointer = mui.MQtUtil.mainWindow()    
+    win = WndTutorial05()
+    win.setWindowTitle("LightLeBhai")
+    win.setStyle(QtGui.QStyleFactory.create('Cleanlooks'))
+    win.show()
